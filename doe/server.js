f5851bacdf63c5e435bd6d3c36d6e301e9ddbd9e
JoacyMS <joacymsilva@gmail.com>
2020-02-18 14:34:48 -0300

    Adição de doadores via formulário, sem persitência dos dados

--- doe/server.js
+++ doe/server.js
@@ -3,13 +3,48 @@ const server = express();
 
 server.use(express.static("public"));
 
+server.use(express.urlencoded({ extended: true }));
+
 const nunjucks = require("nunjucks");
 nunjucks.configure("./", {
     express: server,
+    noCache: true,
 });
 
-server.get("/", function(req, res){
-    return res.render("index.html");
+const donors = [
+    {
+        name: "Diego Fernandes",
+        blood: "AB+",
+    },
+    {
+        name: "Cleiton Souza",
+        blood: "B+",
+    },
+    {
+        name: "Robson Marques",
+        blood: "A+",
+    },
+    {
+        name: "Mayk Brito",
+        blood: "O+",
+    },
+];
+
+server.get("/", function (req, res) {
+    return res.render("index.html", { donors });
+});
+
+server.post("/", function (req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const bloodType = req.body.bloodType;
+
+    donors.push({
+        name: name,
+        blood: bloodType,
+    });
+
+    return res.redirect("/");
 });
 
 server.listen(3333);
